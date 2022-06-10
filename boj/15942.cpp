ca5c21011b94eea7474d1c330befc80ec4997633
yooj-hub <jang0929@naver.com>
2022-06-10 17:19:11 +0900

    0610

--- boj/15942.cpp
+++ boj/15942.cpp
@@ -4,62 +4,69 @@
 #define MX 202020
 
 using namespace std;
-int n, k, p;
+
 int arr[MX];
-bitset<MX> chk;
+int n, k, p;
+int asc, descendent;
+int counter = 1;
 
-bool is_heap() {
-    for (int i = 1; i <= n; i++) {
-        int left = i * 2;
-        int right = i * 2 + 1;
-        if (left <= n) {
-            if (arr[left] < arr[i])
-                return false;
-        }
-
-        if (right <= n)
-            if (arr[i] > arr[right])
-                return false;
+bool make_tree(int idx) {
+    if (descendent > n) {
+        return false;
     }
-    return true;
+    arr[idx] = descendent++;
+    bool ok = true;
+    if (ok && idx * 2 <= n)
+        ok &= make_tree(idx * 2);
+    
+    if (ok && idx * 2 + 1 <= n)
+        ok &= make_tree(idx * 2 + 1);
+    return ok;
 }
-
+void heapify(int idx) {
+    while (idx > 1) {
+        if (arr[idx] < arr[idx / 2])
+            swap(arr[idx], arr[idx / 2]);
+        idx /= 2;
+    }
+}
+void insert(int idx) {
+    if (counter == asc)
+        counter = descendent;
+    arr[idx] = counter++;
+    heapify(idx);
+    if (idx * 2 <= n)
+        insert(idx * 2);
+    if (idx * 2 + 1 <= n)
+        insert(idx * 2 + 1);
+}
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
     cin >> n >> k >> p;
-    int q = 1;
-    int sz = p;
-    while (sz > 1) {
-        q++;
-        sz /= 2;
-    }
-    if (q > k) {
+    descendent = k;
+    if (!make_tree(p)) {
         cout << -1;
-    } else {
-        int current = q - 1;
-        while (p) {
-            if (chk[k] == 0) {
-                arr[p] = k;
-                chk[k] = 1;
-            } else {
-                arr[p] = current;
-                chk[current--] = 1;
-            }
-            p /= 2;
-        }
-        current = 1;
-        for (int i = 1; i <= n; i++) {
-            while (chk[current]==1) {
-                current++;
-            }
-            if (arr[i] == 0) {
-                cout << current << '\n';
-                chk[current] = 1;
-            } else {
-                cout << arr[i] << '\n';
-            }
-        }
+        return 0;
     }
+    asc = k;
+    while (p > 1) {
+        p /= 2;
+        arr[p] = --asc;
+    }
+    if (asc <= 0) {
+        cout << -1;
+        return 0;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (arr[i])
+            continue;
+        insert(i);
+    }
+
+    for (int i = 1; i <= n; i++)
+        cout << arr[i] << '\n';
+    return 0;
 }
---
+++ boj/17469.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define MX 101010
+
+using namespace std;
+
+struct Query {
+    int x, s;
+};
+int n, q,t;
+vector<int> answer;
+vector<Query> quries;
+vector<int> tree;
+int parent[MX];
+unordered_set<int> color_set[MX];
+
+
+int find_parent(int x) {
+    if (x == parent[x])
+        return x;
+    return parent[x] = find_parent(parent[x]);
+}
+
+void union_parent(int a, int b) {
+    a = find_parent(a);
+    b = find_parent(b);
+    if (a == b)
+        return;
+    if (color_set[a].size() > color_set[b].size())
+        swap(a, b);
+    parent[a] = b;
+    for (auto iter = color_set[a].begin(); iter != color_set[a].end(); iter++) {
+        color_set[b].insert(*iter);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> n >> q;
+    tree.resize(n + 1);
+    for (int i = 1; i < n; i++) {
+        cin >> t;
+        tree[i + 1] = t;
+    }
+    for (int i = 1; i <= n; i++) {
+        cin >> t;
+        parent[i] = i;
+        color_set[i].insert(t);
+    }
+
+    for (int i = 0; i < q + n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        quries.push_back({a, b});
+    }
+    for (int i = 1; i <= n; i++) {
+
+    }
+
+    reverse(all(quries));
+    for (Query que : quries) {
+        if (que.x == 1) {
+            union_parent(que.s, tree[que.s]);
+        } else {
+            answer.push_back(color_set[find_parent(que.s)].size());
+        }
+    }
+    reverse(all(answer));
+    for (auto x : answer) {
+        cout << x << '\n';
+    }
+}
